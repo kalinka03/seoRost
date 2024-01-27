8d618e884d87848dc632880fc4a730f99f4a9a9b
kalinka03 <kalinka03333@gmail.com>
2024-01-27 12:43:46 +0300

    404

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php include("template/header.php") ?>
+
+
+<div class="error-page">
+    <div class="container">
+        <div class="error-page__info">
+            <p class="sub-title">Запрашиваемая страница не найдена</p>
+            <p>Возможно был введен некорректный адрес или страница была удалена</p>
+        </div>
+        <h1>404</h1>
+        <div class="error-page__come-back">
+            <a href="/" class="error-page__button">
+                Перейти на главную страницу
+            </a>
+            <div class="error-page__text">
+                <p>Для возврата на предыдущую страницу нажмите кнопку &quot;Назад&quot; в своем браузере</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include("template/popUps.php") ?>
+<?php include("template/footer.php") ?>
+<link rel="stylesheet" type="text/css" href="/assets/css/pages/404.css">
+<script type="text/javascript" src="/js/404.js"></script>
+</body>
+</html>
